94a415f601dcb6543ecb7c92e68a4c41a021bfdc
Mohitcodingz <Mohitsingh2003uk@gmail.com>
2026-07-16 17:18:41 +0530

    done

--- src/component/Navbar/navbar.jsx
+++ src/component/Navbar/navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styles from './navbar.module.css'
+export default function Navbar() {
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.leftSide}>
+                BRANDKART
+            </div>
+            <div className={styles.rightSide}>
+                <a href="#Shop">Shop</a>
+                <a href="#Category">Category</a>
+                <a href="#Cart">Cart</a>
+                <a href="#Login">Login</a>
+            </div>
+        </div>
 
-export default function navbar() {
-  return (
-    <div>navbar</div>
-  )
+    )
 }
