bf986baf1219f51c09a6ae8de74355185bd341ad
mohitcodingz <mohitsingh2003uk@gmail.com>
2026-07-19 22:35:56 +0530

    done

--- src/component/User/user.jsx
+++ src/component/User/user.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import styles from './user.module.css'
 export default function User() {
   return (
-    <div className={Style.userContainer}>
+    <div className={styles.userContainer}>
+    <div className={styles.userContainerInner}>
       <h1>Member Login</h1>
       <form action="/get">
         <input type="text" placeholder='Username' />
-        <input type="password" placeholder='password' />
-        <Button>Login</Button>
+        <input type="password" placeholder='Password' />
+        <button>Login</button>
         <p>Forget Password? <Link to="/">Click Here</Link> </p>
-        <Button >Register</Button>
+        <button>Register</button>
       </form>
+    </div>
     </div>
   )
 }
